b5ca44aba24b5c69f76c7af303656aa09e776b84
Vinicius <vinicius2197@icloud.com>
2022-08-24 10:26:31 -0300

    adicao de exercicio javascript

---
+++ fundamentos/04-introducao-a-javascript-e-logica-de-programacao/01-javascript-primeiros-passos/01-javascript-operacoes-aritmeticas/script.js
@@ -0,0 +1,19 @@
+function adicao(a, b) {
+    return a + b;
+}
+
+function subtrcao(a, b) {
+    return a - b;
+}
+
+function multiplica(a, b) {
+    return a * b;
+}
+
+function divisao(a, b) {
+    return a / b;
+}
+
+function modulo(a, b) {
+    return a % b; 
+}
